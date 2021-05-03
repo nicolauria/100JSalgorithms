89cf61691d4d7397cc37c78feac3fe584c197042
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-03 11:43:13 -0700

    addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,19 @@
 export function addBorder(picture: string[]): string[] {
+    const lengthOfWall = picture[0].length + 2;
+    let wall = '';
 
+    for (let i = 0; i < lengthOfWall; i++) {
+        wall = wall.concat("*");
+    }
+
+    picture.unshift(wall);
+    picture.push(wall);
+
+    for (let i = 1; i < picture.length - 1; i++) {
+        picture[i] = "*" + picture[i] + "*";
+    }
+
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
