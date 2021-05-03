d6a9121c8f9783e242bb67eb036677f0ee369be2
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-03 13:51:18 -0700

    alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,14 @@
 export function alphabeticShift(inputString: string): string {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let result = "";
 
+    for (let i = 0; i < inputString.length; i++) {
+        let index = alphabet.indexOf(inputString[i]);
+        let newIndex = (index + 1) % 26;
+        result += alphabet[newIndex];
+    }
+
+    return result;
 }
 
 console.log(alphabeticShift('crazy'));
