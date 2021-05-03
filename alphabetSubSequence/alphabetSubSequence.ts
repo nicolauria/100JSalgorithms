92e06f3e6ec061b3361945faf909e92cc54318b4
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-03 15:31:32 -0700

    alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,12 @@
 export function alphabetSubsequence(s: string): boolean {
+    let res = s.split("")
 
+    return JSON.stringify(res) === JSON.stringify(res.sort()) && 
+        JSON.stringify(res) === JSON.stringify(res.filter(distinct));
+}
+
+const distinct = (value, index, self) => {
+    return self.indexOf(value) === index;
 }
 
 console.log(alphabetSubsequence('zab'))
