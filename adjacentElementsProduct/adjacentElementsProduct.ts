b0252e577ebfea8d4a3d59d0ee8df778966bc26d
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-03 11:58:25 -0700

    adjacentElementsProducts

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,10 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+    let max = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        let product = inputArray[i] * inputArray[i + 1];
+        max = product > max ? product : max;
+    }
+    return max;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
