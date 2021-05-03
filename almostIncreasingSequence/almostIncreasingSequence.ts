be88b552b9516654f1cf3cd0ffd19b920c789ee3
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-03 12:28:38 -0700

    almostIncreasingSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,23 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+    for (let i = 0; i < sequence.length; i++) {
 
+        let modifiedSequence = [...sequence];
+        modifiedSequence.splice(i, 1);
+
+        let validSequence = true;
+        for (let i = 0; i < modifiedSequence.length - 1; i++) {
+            if (modifiedSequence[i] >= modifiedSequence[i + 1]) {
+                validSequence = false;
+            }
+        }
+        
+        if (validSequence) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
