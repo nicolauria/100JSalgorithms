04f20e169babe0267f955664f99c9bf93267120a
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-03 15:35:45 -0700

    alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,15 @@
 export function alternatingSums(a: number[]): number[] {
+    let totalWeights = [0, 0];
 
+    for (let i = 0; i < a.length; i++) {
+        if (i % 2 === 0) {
+            totalWeights[0] += a[i];
+        } else {
+            totalWeights[1] += a[i];
+        }
+    }
+
+    return totalWeights;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
