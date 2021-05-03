f3e2f869abfcf3b5af27403929cbd4bbc08aa792
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-03 11:54:43 -0700

    addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,12 @@
 export function addTwoDigits(n: any): number {
+    let numString = n.toString();
+    let res = 0;
 
+    for (let i = 0; i < numString.length; i++) {
+        res += parseInt(numString[i])
+    }
+
+    return res;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
