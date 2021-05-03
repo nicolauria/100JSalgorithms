23a0b2be5782e71dfd9b45aa6084c5d054ea8f73
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-03 12:06:01 -0700

    allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+    let max = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        let length = inputArray[i].length;
+        max = length > max ? length : max;
+    }
 
+    let res = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i].length === max) {
+            res.push(inputArray[i]);
+        }
+    }
+
+    return res;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
