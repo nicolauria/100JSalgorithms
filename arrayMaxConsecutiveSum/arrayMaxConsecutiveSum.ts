8351a42e55f498fb130a79be4d96f15f76e6e80d
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-05 16:45:33 -0700

    arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,12 @@
 export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+    let max = Number.NEGATIVE_INFINITY;
 
+    for (let i = 0; i < inputArray.length - k + 1; i++) {
+        let total = inputArray.slice(i, i + k).reduce((a, b) => a + b);
+        max = total > max ? total : max;
+    }
+
+    return max;
 }
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
