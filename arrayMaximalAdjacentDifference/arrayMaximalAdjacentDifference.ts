fd6402d533534d553b2c1586fe97472c661ec0d9
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-05 16:50:10 -0700

    arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,13 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let max = Number.NEGATIVE_INFINITY;
 
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        let total = inputArray.slice(i, i + 2).reduce((a, b) => a - b);
+        let abs = Math.abs(total);
+        max = abs > max ? abs : max;
+    }
+
+    return max;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
