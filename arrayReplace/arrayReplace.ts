95d04c4ad857e09ccf897888f4edb817ddddf95c
DESKTOP-FMNCJIB\nflau <nicolauria@uoregon.edu>
2021-05-05 17:17:54 -0700

    arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,8 @@
 export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-
+    return inputArray.reduce((a, b) => {
+        a.push(b === elemToReplace ? substitutionElem : b);
+        return a;
+    }, [])
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
